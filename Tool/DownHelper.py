3d957e83020c56142f62cef860a34093de7cafe9
ZijianYang <yang20040764@163.com>
2017-10-18 11:55:49 +0800

    a

--- Tool/DownHelper.py
+++ Tool/DownHelper.py
@@ -16,39 +16,35 @@ def star(dirpath, url, name=None):
     if os.path.exists(path):
         print(path+"已经存在", end="")
     else:
-        if isfile:
-            downfile(url, path)
-        else:
-            downhtml(url, path)
+        try:
+            if isfile:
+                downfile(url, path)
+            else:
+                downhtml(url, path)
+        except:
+            return None
     return path
 
 def downfile(url, path):
     """下载文件"""
     print("下载文件" + url, end="")
-    try:
-        request = urllib.request.urlopen(url, data=None, timeout=60)
-        if request.getcode() == 200:
-            data = request.read()
-            with open(path, 'wb') as filestream:
-                filestream.write(data)
-            print("下载完成", end="")
-        else:
-            print(path+"下载失败", end="")
-    except:
-        print("下载异常:")
-
+    request = urllib.request.urlopen(url, data=None, timeout=60)
+    if request.getcode() == 200:
+        data = request.read()
+        with open(path, 'wb') as filestream:
+            filestream.write(data)
+        print("下载完成", end="")
+    else:
+        print(path+"下载失败", end="")
 
 def downhtml(url, path):
     """下载网页"""
     print("下载网页" + url, end="")
-    try:
-        page = urllib.request.urlopen(url, data=None, timeout=60)
-        html = page.read()
-        with open(path, "wb+") as filestream:
-            filestream.write(html)
-        print("下载完成", end="")
-    except:
-        print("下载异常")
+    page = urllib.request.urlopen(url, data=None, timeout=60)
+    html = page.read()
+    with open(path, "wb+") as filestream:
+        filestream.write(html)
+    print("下载完成", end="")
 
 def urltoname(url):
     """urltoname"""
