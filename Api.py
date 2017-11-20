2978ee65f1f96215306e6876e2deabafe9c102d6
ZijianYang <yang20040764@163.com>
2017-11-20 10:41:12 +0800

    step-10

--- Api.py
+++ Api.py
@@ -1,25 +1,29 @@
 """接口"""
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, jsonify, request
 from flask_script import Manager
 from Tool import ClassToDict
 import Store
 import Store.Entity
 
 APP = Flask(__name__,static_folder='', static_url_path='')
+
+#解决Jinja2与Vue.js的模板冲突 解决思路也比较简单，就是在需要Jinja2渲染的时候添加一个空格，而vue.js渲染的时候则不需要空格，python脚本如下
+APP.jinja_env.variable_start_string = '{{ '
+APP.jinja_env.variable_end_string = ' }}'
 MANAGER = Manager(APP)
 
 
 @APP.route('/')
 def index():
     """首页"""
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', name='test')
 
 
 @APP.route('/images/<pageindex>')
 def images(pageindex):
     """查询"""
     args = request.args
-    tag = args.get("tag")
+    tag = None if args.get("tag") == "" else args.get("tag")
     pageindex = int(pageindex)
     pagesize = int(args.get("pagesize")) if args.get("pagesize") else 10
     score = int(args.get("score")) if args.get("score") else 0
@@ -29,7 +33,7 @@ def images(pageindex):
     #print(ClassToDict.todict(data["list"][0]))
     return jsonify({
         'list': [{
-            'filepath': item.filepath.replace("\\","/"),
+            'filepath': item.filepath.replace("\\", "/"),
             'md5': item.md5,
             'score': item.remark1,
             'tags': item.remark2
@@ -38,6 +42,13 @@ def images(pageindex):
         data["total"]
     })
 
+@APP.errorhandler(404)
+def page_not_fount(e):
+    return render_template('404.html'), 404
+
+@APP.errorhandler(500)
+def page_not_fount(e):
+    return render_template('404.html'), 500
 
 if __name__ == '__main__':
     MANAGER.run()
